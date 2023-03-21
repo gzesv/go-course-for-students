b90425af4cfd96b485d9cf405c9b94dd41a5daca
gzevs <grom_zetom@mail.ru>
2023-03-21 21:39:46 +0400

    HW4

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -7,6 +7,9 @@ import (
 	"sync/atomic"
 )
 
+var fileCount int64
+var sizeFile int64
+
 // Result represents the Size function result
 type Result struct {
 	// Total Size of File objects
@@ -33,9 +36,6 @@ func NewSizer() DirSizer {
 	return &sizer{}
 }
 
-var fileCount int64
-var sizeFile int64
-
 func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
 	a.maxWorkersCount = 4
 	runtime.GOMAXPROCS(a.maxWorkersCount)
@@ -58,35 +58,20 @@ func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
 			err = a.getFileSize(file, ctx)
 		})
 	}()
+
 	a.wg.Add(1)
 	go func() {
 		defer a.wg.Done()
 		err = a.walkDir(dir, ctx)
 	}()
-	/*a.wg.Add(1)
-	go func() {
-		defer a.wg.Done()
-		for size := range fileSize {
-			fileCount++
-			sizeFile += size
-			time.Sleep(150 * time.Nanosecond)
-		}
-		close(fileSize)
-	}()*/
 
 	a.wg.Wait()
-	/*time.Sleep(1000 * time.Millisecond)
-	for size := range fileSize {
-		fileCount++
-		sizeFile += size
-	}*/
+
 	return Result{Size: sizeFile, Count: fileCount}, err
 }
 
 func (a *sizer) getFileSize(file []File, ctx context.Context) error {
 	defer a.wg.Done()
-	//time.Sleep(150 * time.Nanosecond)
-
 	a.wg.Add(1)
 	for _, st := range file {
 		s, err := st.Stat(ctx)
@@ -96,8 +81,6 @@ func (a *sizer) getFileSize(file []File, ctx context.Context) error {
 		if err != nil {
 			return err
 		}
-		//r <- Result{s, 1}
-		//r <- s
 		atomic.AddInt64(&fileCount, 1)
 		atomic.AddInt64(&sizeFile, s)
 	}
@@ -107,9 +90,8 @@ func (a *sizer) getFileSize(file []File, ctx context.Context) error {
 func (a *sizer) walkDir(d []Dir, ctx context.Context) error {
 	defer a.wg.Done()
 	a.wg.Add(1)
-	//time.Sleep(150 * time.Nanosecond)
-	for k := 0; k < len(d); k++ {
 
+	for k := 0; k < len(d); k++ {
 		dir, file, err := d[k].Ls(ctx)
 		if err != nil {
 			return err
@@ -122,12 +104,10 @@ func (a *sizer) walkDir(d []Dir, ctx context.Context) error {
 			return err
 		}
 		if dir != nil {
-
 			err = a.walkDir(dir, ctx)
 			if err != nil {
 				return err
 			}
-			//time.Sleep(150 * time.Nanosecond)
 		}
 	}
 	return nil
