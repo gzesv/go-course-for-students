afc3db90365be399956c34125dd2eab46aed0e2b
gzevs <grom_zetom@mail.ru>
2023-03-26 17:05:40 +0400

    HW4

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -38,9 +38,9 @@ func NewSizer() DirSizer {
 }
 
 func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
-	a.maxWorkersCount = 4
+	a.maxWorkersCount = 1
 	runtime.GOMAXPROCS(a.maxWorkersCount)
-
+	//time.Sleep(500 * time.Millisecond)
 	fileCount = 0
 	sizeFile = 0
 	dir, file, err := d.Ls(ctx)
@@ -49,10 +49,6 @@ func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
 	}
 	if file == nil {
 		return Result{}, errors.New("file does not exist")
-	}
-	err = a.walkDir(dir, ctx)
-	if err != nil {
-		return Result{}, err
 	}
 	a.wg.Add(1)
 	go func() {
@@ -67,26 +63,25 @@ func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
 			atomic.AddInt64(&sizeFile, s)
 		}
 	}()
+
+	err = a.walkDir(dir, ctx)
+	if err != nil {
+		return Result{}, err
+	}
+
+	//<-ctx.Done()
 	/*a.wg.Add(1)
 	go func() {
 		defer a.wg.Done()
 		err = er
 	}()
 	*/
-
 	a.wg.Wait()
 	if err != nil {
 		return Result{}, err
 	}
 	return Result{Size: sizeFile, Count: fileCount}, err
 }
-
-/*func (a *sizer) getFileSize(file []File, ctx context.Context) error {
-	defer a.wg.Done()
-	a.wg.Add(1)
-
-	return nil
-}*/
 
 func (a *sizer) walkDir(d []Dir, ctx context.Context) error {
 	var rr error
@@ -103,23 +98,23 @@ func (a *sizer) walkDir(d []Dir, ctx context.Context) error {
 				rr = errors.New("file does not exist")
 				return
 			}
-			//a.wg.Add(1)
-			//go func() {
-			//defer a.wg.Done()
-			for _, st := range file {
-				s, er := st.Stat(ctx)
-				if file == nil {
-					rr = errors.New("file does not exist")
-					return
+			a.wg.Add(1)
+			go func() {
+				defer a.wg.Done()
+				for _, st := range file {
+					s, er := st.Stat(ctx)
+					if file == nil {
+						rr = errors.New("file does not exist")
+						return
+					}
+					if er != nil {
+						rr = er
+						return
+					}
+					atomic.AddInt64(&fileCount, 1)
+					atomic.AddInt64(&sizeFile, s)
 				}
-				if er != nil {
-					rr = er
-					return
-				}
-				atomic.AddInt64(&fileCount, 1)
-				atomic.AddInt64(&sizeFile, s)
-			}
-			//}()
+			}()
 			//err = a.getFileSize(file, ctx)
 			if rr != nil {
 				return
