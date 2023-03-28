b1664b56a221364676bbddb099f3a2d713216218
gzevs <grom_zetom@mail.ru>
2023-03-28 10:40:25 +0400

    HW5

--- lesson5/homework/pipeline.go
+++ lesson5/homework/pipeline.go
@@ -12,5 +12,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(ctx context.Context, in In, stages ...Stage) Out {
-	return nil
+	s := func(ctx context.Context, in In) Out {
+		out := make(chan any)
+		go func() {
+			defer close(out)
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case val, ok := <-in:
+					if !ok {
+						return
+					}
+					select {
+					case <-ctx.Done():
+						return
+					case out <- val:
+					}
+				}
+			}
+		}()
+		return out
+	}
+
+	out := s(ctx, in)
+	for _, stage := range stages {
+		out = stage(s(ctx, out))
+	}
+	return out
 }
