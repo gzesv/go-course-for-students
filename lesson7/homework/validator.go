adc94c227a3ca64d8beffca5e9408ce0d9cb30f7
gzevs <grom_zetom@mail.ru>
2023-04-08 10:48:43 +0400

    HW-7

--- lesson7/homework/validator.go
+++ lesson7/homework/validator.go
@@ -6,7 +6,6 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-	"unicode"
 )
 
 var ErrNotStruct = errors.New("wrong argument given, should be a struct")
@@ -31,41 +30,13 @@ func (v ValidationErrors) Error() string {
 func Validate(v any) error {
 	var vErrors ValidationErrors
 
-	val := reflect.ValueOf(v)
+	value := reflect.ValueOf(v)
 
-	if val.Kind() != reflect.Struct {
+	switch value.Kind() {
+	case reflect.Struct:
+		vErrors = validateStruct(value, vErrors)
+	default:
 		return ErrNotStruct
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		tag := val.Type().Field(i).Tag.Get("validate")
-
-		if len(tag) == 0 {
-			continue
-		}
-
-		valueOf := val.Field(i)
-
-		fieldName := val.Type().Field(i).Name
-
-		if unicode.IsLower(rune(fieldName[0])) {
-			vErrors = append(vErrors, appendError(fieldName, ErrValidateForUnexportedFields))
-			continue
-		}
-
-		var values []interface{}
-		values = getCollectValues(values, valueOf)
-
-		if valueOf.Kind() == reflect.Slice {
-			b := validateSlice(values, tag)
-			if !b {
-				vErrors = append(vErrors, appendError(fieldName, ErrInvalidValidatorSyntax))
-			}
-		} else {
-			for _, value := range values {
-				vErrors = append(vErrors, valIntAndStr(value, fieldName, tag)...)
-			}
-		}
 	}
 
 	if len(vErrors) == 0 {
@@ -75,143 +46,126 @@ func Validate(v any) error {
 	return vErrors
 }
 
-func getCollectValues(values []interface{}, value reflect.Value) []interface{} {
-	switch value.Kind() {
-	case reflect.Slice:
-		for j := 0; j < value.Len(); j++ {
-			values = append(values, value.Index(j).Interface())
-		}
-		return values
-	default:
-		values = append(values, value.Interface())
-		return values
+func validateStruct(value reflect.Value, vErrors ValidationErrors) ValidationErrors {
+	if value.Kind() != reflect.Struct {
+		vErrors = append(vErrors, appendError("", ErrNotStruct))
+		return vErrors
 	}
+
+	for i := 0; i < value.Type().NumField(); i++ {
+		valueField := value.Field(i)
+
+		if valueField.Kind() == reflect.Struct {
+			vErrors = validateStruct(valueField, vErrors)
+		}
+
+		valueTypeField := value.Type().Field(i)
+
+		tag := valueTypeField.Tag.Get("validate")
+		if len(tag) == 0 {
+			continue
+		}
+
+		if !valueTypeField.IsExported() {
+			vErrors = append(vErrors, appendError(valueTypeField.Name, ErrValidateForUnexportedFields))
+			continue
+		}
+
+		switch valueField.Kind() {
+		case reflect.Slice:
+			for j := 0; j < valueField.Len(); j++ {
+				ok := validateValue(valueTypeField, valueField.Index(j))
+				if !ok {
+					vErrors = append(vErrors, appendError(valueTypeField.Name, ErrInvalidValidatorSyntax))
+					break
+				}
+			}
+		default:
+			ok := validateValue(valueTypeField, valueField)
+			if !ok {
+				vErrors = append(vErrors, appendError(valueTypeField.Name, ErrInvalidValidatorSyntax))
+			}
+		}
+
+	}
+	return vErrors
 }
-func validateSlice(values []interface{}, tag string) bool {
+
+func validateValue(valueTypeField reflect.StructField, value reflect.Value) bool {
+	tag := valueTypeField.Tag.Get("validate")
 	rulesArr := strings.SplitN(tag, ":", 2)
 	rName := rulesArr[0]
 	rVal := rulesArr[1]
 
 	switch rName {
 	case "len":
-		oVal, err := strconv.ParseInt(rVal, 10, 64)
-		if err != nil {
-			return false
-		}
-		for _, v := range values {
-			if len([]rune(interfaceToString(v))) != int(oVal) {
+		switch value.Kind() {
+		case reflect.String:
+			rVal, err := strconv.ParseInt(rVal, 10, 64)
+			if err != nil {
 				return false
 			}
+			return int64(len([]rune(value.String()))) == rVal
 		}
-		return true
+		return false
 	case "max":
-		_, err := strconv.ParseInt(rVal, 10, 64)
-		if err != nil {
-			return false
-		}
-		for _, v := range values {
-			if rVal < interfaceToString(v) {
+		switch value.Kind() {
+		case reflect.String:
+			rVal, err := strconv.ParseInt(rVal, 10, 64)
+			if err != nil {
 				return false
 			}
+			return int64(len([]rune(value.String()))) <= rVal
+		case reflect.Int:
+			rVal, err := strconv.ParseInt(rVal, 10, 64)
+			if err != nil {
+				return false
+			}
+			return value.Int() <= rVal
 		}
-		return true
+		return false
 	case "min":
-		_, err := strconv.ParseInt(rVal, 10, 64)
-		if err != nil {
-			return false
-		}
-		for _, v := range values {
-			if rVal > interfaceToString(v) {
+		switch value.Kind() {
+		case reflect.String:
+			rVal, err := strconv.ParseInt(rVal, 10, 64)
+			if err != nil {
 				return false
 			}
+			return int64(len([]rune(value.String()))) >= rVal
+		case reflect.Int:
+			rVal, err := strconv.ParseInt(rVal, 10, 64)
+			if err != nil {
+				return false
+			}
+			return value.Int() >= rVal
 		}
-		return true
+		return false
 	case "in":
-		var counter int
-		if rVal == "" {
-			return false
-		}
-		for _, v := range values {
+		switch value.Kind() {
+		case reflect.String:
+			if rVal == "" {
+				return false
+			}
 			for _, s := range strings.Split(rVal, ",") {
-				if s == interfaceToString(v) {
-					counter++
-					break
+				if s == value.String() {
+					return true
 				}
 			}
-		}
-		return len(values) == counter
-	}
-	return false
-}
-
-func valIntAndStr(value interface{}, name string, rules string) ValidationErrors {
-	var vError ValidationErrors
-
-	rulesArr := strings.SplitN(rules, ":", 2)
-	rName := rulesArr[0]
-	rVal := rulesArr[1]
-
-	ok := validateValue(interfaceToString(value), rName, rVal)
-	if !ok {
-		vError = append(vError, appendError(name, ErrInvalidValidatorSyntax))
-	}
-
-	if len(vError) == 0 {
-		return nil
-	}
-
-	return vError
-}
-
-func validateValue(val string, rName string, rVal string) bool {
-	switch rName {
-	case "len":
-		oVal, err := strconv.ParseInt(rVal, 10, 64)
-		if err != nil {
 			return false
-		}
-
-		return len([]rune(val)) == int(oVal)
-	case "max":
-		v, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			oVal, err := strconv.ParseInt(rVal, 10, 64)
-			if err != nil {
+		case reflect.Int:
+			if rVal == "" {
 				return false
 			}
-			return len(val) <= int(oVal)
-		}
-
-		ruleVal, err := strconv.ParseInt(rVal, 10, 64)
-		if err != nil {
-			return false
-		}
-
-		return v <= ruleVal
-	case "min":
-		v, err := strconv.ParseInt(val, 10, 64)
-		if err != nil {
-			oVal, err := strconv.ParseInt(rVal, 10, 64)
-			if err != nil {
-				return false
+			for _, s := range strings.Split(rVal, ",") {
+				s, err := strconv.ParseInt(s, 10, 64)
+				if err != nil {
+					return false
+				}
+				if s == value.Int() {
+					return true
+				}
 			}
-			return len(val) >= int(oVal)
-		}
-
-		ruleVal, err := strconv.ParseInt(rVal, 10, 64)
-		if err != nil {
 			return false
-		}
-
-		return v >= ruleVal
-	case "in":
-		if rVal == "" {
-			return false
-		}
-		for _, s := range strings.Split(rVal, ",") {
-			if s == val {
-				return true
-			}
 		}
 		return false
 	}
@@ -223,15 +177,4 @@ func appendError(name string, err error) ValidationError {
 		Name: name,
 		Err:  err,
 	}
-}
-
-func interfaceToString(i interface{}) string {
-	format := "%s"
-	switch i.(type) {
-	case float32, float64:
-		format = "%f"
-	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
-		format = "%d"
-	}
-	return fmt.Sprintf(format, i)
 }
